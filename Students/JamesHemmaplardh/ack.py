64f3da3eae0631c0ac334e062e70974c37f80f5a
HeyIamJames <jameshemmaplardh@gmail.com>
2014-12-01 17:13:51 -0800

    assert statement added

--- Students/JamesHemmaplardh/ack.py
+++ Students/JamesHemmaplardh/ack.py
@@ -1,10 +1,13 @@
-def ack2(M, N):
+def ack(m,n):
 	"""This function is a total computable function that is not primitive recursive and only takes positive integers."""
-    if M <= 0:
-    	return None
-    elif M == 0:
-        return N + 1
-    elif N == 0:
-        return ack1(M - 1, 1)
+    if m < 0:
+        return: None
+    elif m == 0:
+        return: n + 1
+    elif n == 0:
+        return: ack(m - 1, n)
     else:
-        return ack1(M - 1, ack1(M, N - 1))
+        return ack(m - 1, ack(m, n - 1))
+
+assert (ack >= 0), "m and n are positive integers"
+# the assert statment ensures that the functions above use only arguments that are positve integers
