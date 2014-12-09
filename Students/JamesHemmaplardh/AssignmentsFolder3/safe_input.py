1a7f9398044bb083338c1f5f865e61dc9401c6c9
HeyIamJames <jameshemmaplardh@gmail.com>
2014-12-08 17:14:17 -0800

    added exception eerrors to return none

--- Students/JamesHemmaplardh/AssignmentsFolder3/safe_input.py
+++ Students/JamesHemmaplardh/AssignmentsFolder3/safe_input.py
@@ -1,10 +1,4 @@
 
-
-'''
-
-The raw_input() function can generate two exceptions:
-EOFError or KeyboardInterrupt on end-of-file (EOF) or canceled input.
-
-Create a wrapper function, perhaps safe_input() that returns
-None rather rather than raising these exceptions, when the user enters ^C for Keyboard Interrupt, or ^D (^Z on Windows) for End Of File.
-'''
+def safe_input()
+    a = raw_input('anything')
+    except (EOFError, KeyboardInterrupt): None
