eceb4707f88a1f6a0ee4f5fb62829ea506474cc6
HeyIamJames <jameshemmaplardh@gmail.com>
2014-12-10 13:54:06 -0800

    Changed/fixed trigram, uses nltk.

--- Students/JamesHemmaplardh/AssignmentsFolder3/trigrams.py
+++ Students/JamesHemmaplardh/AssignmentsFolder3/trigrams.py
@@ -1,6 +1,8 @@
+#this requires NLTK, "sudo pip install -U nltk", assuming you have pip.
+
 from nltk.util import ngrams
-sentence = 'this is a foo bar sentences and i want to ngramize it'
-n = 6
-sixgrams = ngrams(sentence.split(), n)
-for grams in sixgrams:
+sentence = "This is just a short story to prove that this works. It is very boring. Once I walked into a car and drove, then I got fast food at McDonalds. I went home and cried. At the end of the day I realized McDonalds is bad"
+n = 3
+trigrams = ngrams(sentence.split(), n)
+for grams in trigrams:
   print grams
