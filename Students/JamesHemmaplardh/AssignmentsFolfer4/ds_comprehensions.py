5f2cbbf95e112c95b50d2bc0e74512464cc1aca9
HeyIamJames <jameshemmaplardh@gmail.com>
2014-12-15 12:49:38 -0800

    Added string format, range and sets

--- Students/JamesHemmaplardh/AssignmentsFolfer4/ds_comprehensions.py
+++ Students/JamesHemmaplardh/AssignmentsFolfer4/ds_comprehensions.py
@@ -1,7 +1,24 @@
-food_prefs = {"name": u"Cris",
+food_prefs = {u"name": u"Cris",
               u"city": u"Seattle",
               u"cake": u"lemon",
               u"fruit": u"pomegranate",
               u"salad": u"chop",
               u"pasta": u"lasagna"}
-        
+print('{name} is from {city}, his favorite cake is {cake}cake with a side of {fruit} and {salad}, then {name} finishes it off with a zesty {pasta}!'.format(**food_prefs))
+Dict1 = range(16)
+
+s2 = set()
+s3 = set()
+s4 = set()
+for i in range(21):
+    if i % 2 == False:
+        s2.add(i)
+	if i % 3 == False:
+		s3.add(i)
+	if i % 4 == False:
+		s4.add(i)
+
+
+
+#sentence = "%s is from %s " % (name, city)
+
