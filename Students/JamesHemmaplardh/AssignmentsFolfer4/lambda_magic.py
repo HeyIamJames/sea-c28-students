7d01df379823c0195ff66b6f6d7c052a5b41e2db
HeyIamJames <jameshemmaplardh@gmail.com>
2014-12-15 15:08:50 -0800

    Added docstring and if name = main.

--- Students/JamesHemmaplardh/AssignmentsFolfer4/lambda_magic.py
+++ Students/JamesHemmaplardh/AssignmentsFolfer4/lambda_magic.py
@@ -1,5 +1,5 @@
-#make a function to return a list of functions 
-
 x = [(lambda arg: (lambda x: arg + x))(i) for i in range(11)]
+"""Increment list value of [] by ().""" 
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
+    print "test: x[2](1) ==", 3
