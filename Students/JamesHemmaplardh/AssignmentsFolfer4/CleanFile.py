0e68a122a886fdc60920491046029612564b4a87
HeyIamJames <jameshemmaplardh@gmail.com>
2014-12-17 18:53:35 -0800

    fixed some issues

--- Students/JamesHemmaplardh/AssignmentsFolfer4/CleanFile.py
+++ Students/JamesHemmaplardh/AssignmentsFolfer4/CleanFile.py
@@ -3,18 +3,24 @@
 
 import codecs 
 import sys
+import io
+
+InputFile = sys.argv[1]
+OutputFile = sys.argv[2]
 
 def remove_whitespace(InputFile, OutputFile):
     io.open(OutputFile, 'w').writelines( [line.strip()+"\n" for line in io.open(InputFile).readlines()] )
 
 
-UserChoice = raw_input(u"Would you like to write a new OutputFile? Enter 'Yes/No':")
-if UserChoice == "Yes":
-    return: remove_whitespace(InputFile, OutputFile)
-elif UserChoice == "No":
-    return: remove_whitespace(InputFile, InputFile)
-else:
-    print "Enter 'Yes/No'"
+if __name__ == "__main__":
+
+	UserChoice = raw_input(u"Would you like to write a new OutputFile? Enter 'Yes/No':")
+	if UserChoice == "Yes":
+	    remove_whitespace(InputFile, OutputFile)
+	elif UserChoice == "No":
+	    remove_whitespace(InputFile, InputFile)
+	else:
+	    print "Enter 'Yes/No'"
 
 
 
