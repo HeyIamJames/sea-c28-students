043998ee55f27a354a5baabaff9ef4a631185be3
HeyIamJames <jameshemmaplardh@gmail.com>
2014-12-18 12:59:38 -0800

    created a list comprehension, tested and works

--- Students/JamesHemmaplardh/AssignmentsFolfer4/count_evens.py
+++ Students/JamesHemmaplardh/AssignmentsFolfer4/count_evens.py
@@ -1,7 +1,4 @@
-def count_evens(nums):
 """return the number of even numbers in a given array"""
-    x = 0
-    for i in nums:
-        if i % 2 == 0:
-            x += 1
-    return x
+def count_evens(nums):
+	return len([i for i in nums if i % 2 ==0])
+
