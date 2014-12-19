7e3ec7315fd0709c05357121eb0d16fd627afe4c
HeyIamJames <jameshemmaplardh@gmail.com>
2014-12-19 15:08:28 -0800

    added a few lines, better structure

--- Students/JamesHemmaplardh/AssignmentsFolder6/timing_context_manager.py
+++ Students/JamesHemmaplardh/AssignmentsFolder6/timing_context_manager.py
@@ -5,12 +5,9 @@ def foo():
  	for i in range(100000):
  		i = i ** 20
 
-
-start = time.time()
-#def foo()
-end = time.time()
-print end - start
-
-
-
+if __name__ == "__main__":
+	start = time.time()
+	foo()
+	end = time.time()
+	print "elapsted time(sec):", end - start 
 
