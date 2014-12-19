2bd2a0e59142b0c8ecd40686c74c4fcafbabb706
HeyIamJames <jameshemmaplardh@gmail.com>
2014-12-19 14:44:08 -0800

    created meathod to calculate time to run function

--- Students/JamesHemmaplardh/AssignmentsFolder6/timing_context_manager.py
+++ Students/JamesHemmaplardh/AssignmentsFolder6/timing_context_manager.py
@@ -1,7 +1,16 @@
 """Print elapsed time to run code."""
 import time
-def timer():
 
 def foo():
-	for i in range(100000):
-		i = i ** 20
+ 	for i in range(100000):
+ 		i = i ** 20
+
+
+start = time.time()
+#def foo()
+end = time.time()
+print end - start
+
+
+
+
