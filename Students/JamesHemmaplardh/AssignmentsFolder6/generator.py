2837f7753df9713d3d3bf41bdc9f834ce3b2ec0f
HeyIamJames <jameshemmaplardh@gmail.com>
2014-12-19 21:08:21 -0800

    all tests pass

--- Students/JamesHemmaplardh/AssignmentsFolder6/generator.py
+++ Students/JamesHemmaplardh/AssignmentsFolder6/generator.py
@@ -1,13 +1,39 @@
-def sum_of_integers():
+def intsum():
 	x = 0
 	y = 1
 	while True:
-		yield sum_of_integers
-		x += 1
-		sum_of_integers += x 
+		yield x
+		x = x + y
+		y += 1
+
+def intsum2():
+	x = 0
+	y = 1
+	while True:
+		yield x
+		x = x + y
+		y += 1
 
 def doubler():
 	x = 1
 	while True:
 		yield x
 		x = x * 2
+
+def fib():
+	x = 0
+	y = 1
+	while True:
+		yield y
+		y = y + x
+		x = y - x
+
+def prime():
+	x = 2
+	while True:
+	    for i in range(2, x + 1):
+	    	if x == i:
+	    		yield x
+	    	elif x % i == 0:
+	    		break
+	    x += 1
