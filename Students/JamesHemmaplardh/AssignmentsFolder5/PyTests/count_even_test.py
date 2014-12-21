aba44a19f7945f5007983c95570e976df2c6839a
HeyIamJames <jameshemmaplardh@gmail.com>
2014-12-20 23:05:02 -0800

    added doc string

--- Students/JamesHemmaplardh/AssignmentsFolder5/PyTests/count_even_test.py
+++ Students/JamesHemmaplardh/AssignmentsFolder5/PyTests/count_even_test.py
@@ -1,7 +1,9 @@
 
+"""Test count_evens module."""  
+
 import pytest
 from count_evens import count_evens
 
 def test_evens():
-    l = [1, 2, 3, 4, 5]
-    assert count_evens(l) == 2
+    x = [1, 2, 3, 4, 5]
+    assert count_evens(x) == 2
