5f7e8413d4eeb69744d2aa793bf24ff15f46fb20
HeyIamJames <jameshemmaplardh@gmail.com>
2014-12-21 00:02:08 -0800

    added area and other test passes

--- Students/JamesHemmaplardh/AssignmentsFolder5/circle.py
+++ Students/JamesHemmaplardh/AssignmentsFolder5/circle.py
@@ -12,7 +12,33 @@ class Circle(object):
     def _diamater_(self):
         return self.radius * 2
 
-    def _diamater2_(self, diameter):
+    def _diamateradius2_(self, diameter):
         self.radius = diameter / 2
 
-    diameter = property(_diamater_, _diamater2_)
+    diameter = property(_diamater_, _diamateradius2_)
+ 
+
+    def _get_area(self):
+        return math.pi * (self.radius ** 2)
+
+    area = property(_get_area)
+
+    def __str__(self):
+        string = u'Circle with radius: ' + format(self.radius, '.6f')
+        return string
+
+
+    def __repr__(self):
+        string = u'Circle({0!r})'.format(self.radius)
+        return string
+
+
+    def __add__(self, other):
+        radius1 = self.radius
+        radius2 = other.radius
+        circ = Circle(radius1 + radius2)
+        return circ
+
+
+
+
