47242b4e0c7c9fddc51606b630e5a7d72405b6a3
HeyIamJames <jameshemmaplardh@gmail.com>
2014-12-21 00:58:16 -0800

    simple dict added

---
+++ Students/JamesHemmaplardh/AssignmentsFolder3/mailroom.py
@@ -0,0 +1,11 @@
+Donators = {
+    u"James Hemmaplardh": [10, 50, 900],
+    u"Bill Gates": [999, 999, 999],
+    u"The Batman": [40, 3, 10],
+    u"Dash Berlin": [10, 5],
+    u"Avicii Levels": [69] 
+
+    }
+
+def get_donation(donor):
+
