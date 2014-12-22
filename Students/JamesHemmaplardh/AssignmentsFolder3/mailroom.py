c23f2152c2e394e179abd1f170086e3ca13782e0
HeyIamJames <jameshemmaplardh@gmail.com>
2014-12-21 23:59:22 -0800

    did not finish, does not work, dont look please

--- Students/JamesHemmaplardh/AssignmentsFolder3/mailroom.py
+++ Students/JamesHemmaplardh/AssignmentsFolder3/mailroom.py
@@ -1,11 +1,42 @@
-Donators = {
-    u"James Hemmaplardh": [10, 50, 900],
-    u"Bill Gates": [999, 999, 999],
-    u"The Batman": [40, 3, 10],
-    u"Dash Berlin": [10, 5],
-    u"Avicii Levels": [69] 
+#!/usr/bin/env python
 
-    }
 
-def get_donation(donor):
+# import math
 
+# Donators = {
+#     u"James Hemmaplardh": [10, 50, 900],
+#     u"Bill Gates": [999, 9999, 999],
+#     u"The Batman": [1],
+#     u"Dash Berlin": [10, 5],
+#     u"Avicii Levels": [69, 99, 101] 
+#     }
+
+# user_choice = int(raw_input("Enter '1' or '2', 1 to Send a ThankYou, 2 to Create a Report"))
+
+
+# def choice(user_choice):
+#     if user_choice == 1:
+#         return send_thankyou()
+#     elif user_choice == 2:
+#         return create_report()
+#     else:
+#     	print "Please enter '1' or '2', 1 to Send a ThankYou, 2 to Create a Report"
+
+# def send_thankyou():
+#     while True:
+#     	name = raw_input("Enter First and Last name:")
+#     	if name == #donator list
+
+
+# def thankyou_letter():
+# 	print "Thank you %s for donating %s" (donor_name, donor_ammount)
+
+
+# def create_report():
+#     donations_sum = []
+#     for (x, i) in Donators():
+#     	times = len(i)
+#     	total = sum(i)
+#     	average = (total/times)
+#     	create_report.append(x, times, total, average)
+#     print donations_sum
