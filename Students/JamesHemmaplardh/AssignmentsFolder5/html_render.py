9e3e3f2c26bf0dbe6381ca73cdca3a6985348ac0
HeyIamJames <jameshemmaplardh@gmail.com>
2014-12-21 21:54:43 -0800

    added/fixed missing parts

--- Students/JamesHemmaplardh/AssignmentsFolder5/html_render.py
+++ Students/JamesHemmaplardh/AssignmentsFolder5/html_render.py
@@ -9,36 +9,98 @@ class Element(object):
             self.content = []
         else:
             self.content = [content]
-        
+
         self.attributes = kwargs
 
     def append(self, element):
-    	self.content.append(element)
+        self.content.append(element)
+
+    def render(self, file_out, ind = u"    "):
+        attributes = ''
+        for (key, value) in self.attributes.items():
+            attributes += (' {key} = "{value}"'.format(key = key, value = value))
+        file_out.write(u'\n%s<%s%s>\n' % (ind, self.tag, attributes))
+        for item in self.content:
+            try:
+                item.render(file_out, ind + self.indent)
+            except AttributeError:
+                file_out.write(self.indent + ind)
+                file_out.write(item)
+        file_out.write(u'\n%s</%s>' % (ind, self.tag))
 
 class Html(Element):
-	tag = u"html"
+    tag = u"html"
+
+    def render(self, file_out, ind=u""):
+        print u"hmtl"
+        file_out.write(u"<!DOCTYPE html>")
+        Element.render(self, file_out)
+
 
 class Head(Element):
-	tag = u"head"
+    tag = u"head"
+
 
 class Body(Element):
-	tag = u"body"
+    tag = u"body"
+
 
 class P(Element):
-	tag = u"p"
+    tag = u"p"
 
 class SelfClosingTag(Element):
-	def render(self, file_out, ind = "    "):
-		file_out.write('{}<{}{} />\n'.format(ind, sef.tag, self.attributes))
+    def __init__(self, **attributes):
+        self.attributes = attributes
 
-class A(SelfClosingTag):
+    def render(self, file_out, ind = u"    "):
+        attributes = ''
+        for (key,value) in self.attributes.items():
+            attributes += (' {key} = "{value}"'.format(key = key, value = value))
+        file_out.write(u'\n%s<%s%s>\n' % (ind, self.tag, attributes))
+
+
+class Hr(SelfClosingTag):
+    tag = u"hr"
+
+
+class Title(OneLineTag):
+    tag = u"title"
+
+class OneLineTag(Element):
+
+    def render(self, file_out, ind = u"    "):
+        attributes = '  '
+        for (key,value) in self.attributes.items():
+            attributes += (' {key} = "{value}"'.format(key = key, value = value))
+        file_out.write(u'\n%s<%s%s>\n' % (ind, self.tag, attributes))
+        for item in self.content:
+            try:
+                item.render(file_out, ind + self.indent)
+            except AttributeError:
+                file_out.write(self.indent + ind)
+                file_out.write(unicode(item))
+        file_out.write(u'\n%s</%s>' % (ind, self.tag))
+
+
+class A(OneLineTag):
     tag = u"a"
     def __init__(self, link, content):
         OneLineTag.__init__(self, content, href=link)
 
-class UI(Element):
-	tag = u"ul"
 
-class LI(Element):
-	tag = u"li"
+class H(OneLineTag):
+    def __init__(self, level, content, **attributes):
+        OneLineTag.__init__(self, content, **attributes)
+
+        self.tag = u"h%i"%level
+
+class Ul(Element):
+    tag = u"ul"
+
+
+class Li(Element):
+    tag = u"li"
+
+class M(SelfClosingTag):
+    tag = u"meta"
 
